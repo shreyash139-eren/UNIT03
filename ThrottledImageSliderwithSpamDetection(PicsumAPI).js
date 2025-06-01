4c7a3dc3cd001b1b256305bf770e382bba6c4624
Kumar Shreyash <shreyash.sinha35@gmail.com>
2025-06-01 23:32:20 +0530

    videos

--- ThrottledImageSliderwithSpamDetection(PicsumAPI).js
+++ ThrottledImageSliderwithSpamDetection(PicsumAPI).js
@@ -1,66 +1,87 @@
-
-let Alert=false
-let nextClick=0
-let prevClick=0
-let images=[]
-let CurrPage=1,itemsPerPage=1
+let Alert = false;
+let nextClick = 0;
+let prevClick = 0;
+let images = [];
+let CurrPage = 1
+let itemsPerPage = 1;
 let total;
-async function Image(){
-    try {
-        let container=document.getElementById("container")
-             container.innerHTML=""
-        let res=await fetch("https://picsum.photos/v2/list")
-            images=await res.json()
 
-            total=Math.ceil(images.length/itemsPerPage)
-showImage(CurrPage)
-    } catch (error) {
-         let container=document.getElementById("container")
-             container.innerHTML=error.message
-    }
+async function Image() {
+  try {
+    let container = document.getElementById("container");
+    container.innerHTML = "";
+    let res = await fetch("https://picsum.photos/v2/list");
+    images = await res.json();
+    showImage(CurrPage);
+  } catch (error) {
+    let container = document.getElementById("container");
+    container.innerHTML = error.message;
+  }
 }
 
-let start=CurrPage
+// let start=CurrPage
 
-function showImage(page){
-    let container=document.getElementById("container")
-    container.innerHTML=""
+function showImage(page) {
+  let container = document.getElementById("container");
+  container.innerHTML = "";
 
-    let start=(page-1)*itemsPerPage
-    let end=page*itemsPerPage
-    let pageitems=images.slice(start,end)
-    
-    pageitems.forEach((ele)=>{
-        let img=document.createElement("img")
-        img.src=ele.download_url
-        container.appendChild(img)
-    })
+  let start = (page - 1) * itemsPerPage;
+  let end = page * itemsPerPage;
+  let pageitems = images.slice(start, end);
+
+  pageitems.forEach((ele) => {
+    let img = document.createElement("img");
+    img.src = ele.download_url;
+    container.appendChild(img);
+  });
+}
+
+function updatePage(newPage) {
+  if (newPage < 1 || newPage > total) {
+    return;
+  }
+  CurrPage=newPage
+ 
+  const nextbtn= document.getElementById("next").disabled
+  const prevbtn= document.getElementById("prev").disabled
+  nextbtn.disabled=true
+  prevbtn.disabled=true
+
+  
+  setTimeout(()=>{
+    nextbtn.disabled=false
+    prevbtn.disabled=false
+
+})
+
+  rapidClicks()
+  showImage(CurrPage);
 
 }
 
-function updatePage(newPage){
-    
-    if(newPage<1 || newPage>total){
-        return
-    }
-    CurrPage=newPage
-    setTimeout(()=>{
-        showImage(CurrPage)
-        if((nextClick===3 && !Alert)|| (prevClick===3 && !Alert)){
+
+
+let clicks=[]
+    function rapidClicks(){
+        const now=Date.now()
+        clicks.push(now)
+
+        clicks=clicks.filter(ts => now-ts<1000)
+
+        if(clicks.length>=3){
             alert("Chill chill, loading it!!")
-            Alert=true
+            clicks=[]
         }
-    },1000)
-    Alert=false
-}
-    
+    }
 
-let prev=document.getElementById("prev").addEventListener("click",()=>{
-    updatePage(CurrPage-1)
-    prevClick++
-})
-let next=document.getElementById("next").addEventListener("click",()=>{
-    updatePage(CurrPage+1)
-    nextClick++
-})
 
+let prev = document.getElementById("prev").addEventListener("click", () => {
+  updatePage(CurrPage - 1);
+  prevClick++;
+});
+let next = document.getElementById("next").addEventListener("click", () => {
+  updatePage(CurrPage + 1);
+  nextClick++;
+  console.log(nextClick);
+});
+showImage(CurrPage);
