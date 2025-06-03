c8ad1e7f06b7ec25503df3c8690adc2a9b931286
Kumar Shreyash <shreyash.sinha35@gmail.com>
2025-06-04 00:18:57 +0530

    o

--- RickandMortyWiki/detail.js
+++ RickandMortyWiki/detail.js
@@ -1,16 +1,15 @@
 async function details() {
-    try {
-      const detail = document.getElementById("detail");
-      detail.innerHTML=""
-      const id = localStorage.getItem("id");
-  
-      const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
-      const data = await res.json();
-        console.log(data)
-      const card=document.createElement("div")
-     
-      
-      card.innerHTML=`
+  try {
+    const detail = document.getElementById("detail");
+    detail.innerHTML = "";
+    const id = localStorage.getItem("id");
+
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+    const data = await res.json();
+    console.log(data);
+    const card = document.createElement("div");
+
+    card.innerHTML = `
                 <img src="${data.image}" alt="${data.name}">
                 <p>Name : ${data.name}</p>
                 <p>Status : ${data.status}</p>
@@ -20,32 +19,54 @@ async function details() {
                 <p>Origin Location : ${data.origin.name}</p>
                 <p>Current Location : ${data.location.name}</p>
                 <p>Episode apperances : ${data.episode.length}</p>
-                <p>Character's Endpoint : <a href="${data.url}">${data.url}</a></p>
-                <p>Created : ${data.created}</p>`
+                <p>Character's Endpoint : <a href="${data.url}">${
+      data.url
+    }</a></p>
+                <p>Created : ${data.created}</p>`;
 
-                detail.appendChild(card)
-      
-    } catch (error) {
-      const detail = document.getElementById("detail");
-      const p=document.createElement("p")
-      p.innerText=error.message
-      detail.appendChild(p)
-    }
+    detail.appendChild(card);
+  } catch (error) {
+    const detail = document.getElementById("detail");
+    const p = document.createElement("p");
+    p.innerText = error.message;
+    detail.appendChild(p);
   }
-  const clock=document.getElementById("time")
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["January", "February", "March", "April", "May", "June", 
-                "July", "August", "September", "October", "November", "December"];
+}
+const clock = document.getElementById("time");
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 setInterval(() => {
-    const date=new Date
-    const day=date.getDay()
-    const month=months[date.getMonth()]
-    const year=date.getFullYear()
-    const weekDay=days[date.getDay()]
-    const second=date.getSeconds()
-    const minute=date.getMinutes()
-    const hour=date.getHours()
+  const date = new Date();
+  const day = date.getDay();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+  const weekDay = days[date.getDay()];
+  const second = date.getSeconds();
+  const minute = date.getMinutes();
+  const hour = date.getHours();
 
-    clock.innerText=`${hour}:${minute}:${second} - ${weekDay} - ${day+1} ${month} ${year}`
-
+  clock.innerText = `${hour}:${minute}:${second} - ${weekDay} - ${
+    day + 1
+  } ${month} ${year}`;
 }, 1000);
--- RickandMortyWiki/main.js
+++ RickandMortyWiki/main.js
@@ -3,108 +3,129 @@ let CurrPage = 1;
 let itemsPerPage = 6;
 let total = 1;
 document.addEventListener("DOMContentLoaded", () => {
-async function fetchData() {
-  try {
-    let Page = 1;
-    let res = await fetch(`https://rickandmortyapi.com/api/character?page=1`);
-    let data = await res.json();
-    let totalPages = data.info.pages;
+  async function fetchData() {
+    try {
+      let Page = 1;
+      let res = await fetch(`https://rickandmortyapi.com/api/character?page=1`);
+      let data = await res.json();
+      let totalPages = data.info.pages;
 
-    for (let i = 0; i < totalPages; i++) {
-      let res1 = await fetch(
-        `https://rickandmortyapi.com/api/character?page=${Page}`
-      );
-      let data1 = await res1.json();
-      data1 = data1.results;
-      characters.push(...data1);
-      Page++;
+      for (let i = 0; i < totalPages; i++) {
+        let res1 = await fetch(
+          `https://rickandmortyapi.com/api/character?page=${Page}`
+        );
+        let data1 = await res1.json();
+        data1 = data1.results;
+        characters.push(...data1);
+        Page++;
+      }
+      total = Math.ceil(characters.length / itemsPerPage);
+      showData(CurrPage);
+      document.getElementById("loading").style.display = "none";
+    } catch (error) {
+      console.log(error.message);
     }
-    total = Math.ceil(characters.length / itemsPerPage);
-    showData(CurrPage);
-    document.getElementById("loading").style.display = "none";
-  } catch (error) {
-    console.log(error.message);
   }
-}
 
-function showData(page) {
-  let container = document.getElementById("container");
-  container.innerHTML = "";
+  function showData(page) {
+    let container = document.getElementById("container");
+    container.innerHTML = "";
 
-  let start = (page - 1) * itemsPerPage;
-  let end = page * itemsPerPage;
-  let pageItems = characters.slice(start, end);
+    let start = (page - 1) * itemsPerPage;
+    let end = page * itemsPerPage;
+    let pageItems = characters.slice(start, end);
 
-  pageItems.forEach((ele) => {
-    let card = document.createElement("div");
+    pageItems.forEach((ele) => {
+      let card = document.createElement("div");
 
-    let link = document.createElement("a");
-    link.href = "unique.html";
-    link.textContent = "More Details";
-    link.addEventListener("click", () => {
-      localStorage.setItem("id", ele.id);
-    });
+      let link = document.createElement("a");
+      link.href = "unique.html";
+      link.textContent = "More Details";
+      link.addEventListener("click", () => {
+        localStorage.setItem("id", ele.id);
+      });
 
-    card.innerHTML = ` <img src="${ele.image}" alt="${ele.name}">
+      card.innerHTML = ` <img src="${ele.image}" alt="${ele.name}">
                     <p>Name : ${ele.name}</p>
                     <p>Species : ${ele.species}</p>
                     <p>Status : ${ele.status}</p>
                     `;
-    card.append(link);
-    container.appendChild(card);
-  });
-}
-
-function updatePage(newPage) {
-  if (newPage < 1 || newPage > total) {
-    return;
+      card.append(link);
+      container.appendChild(card);
+    });
   }
-  CurrPage = newPage;
-  showData(CurrPage);
-}
 
-let prev = document.getElementById("prev")
-    prev.addEventListener("click", () => {
-  updatePage(CurrPage - 1);
+  function updatePage(newPage) {
+    if (newPage < 1 || newPage > total) {
+      return;
+    }
+    CurrPage = newPage;
+    showData(CurrPage);
+  }
+
+  let prev = document.getElementById("prev");
+  prev.addEventListener("click", () => {
+    updatePage(CurrPage - 1);
+  });
+
+  let next = document.getElementById("next");
+  next.addEventListener("click", () => {
+    updatePage(CurrPage + 1);
+  });
+
+  function themeSelector() {
+    const theme = document.getElementById("theme").value;
+    document.querySelector("body").style.backgroundColor = theme;
+    showData(CurrPage);
+  }
+
+  function Random() {
+    const Randombtn = document.getElementById("random");
+    Randombtn.addEventListener("click", async () => {
+      window.location.href = "random.html";
+    });
+  }
+
+  Random();
+  document.getElementById("theme").addEventListener("change", themeSelector);
+
+  fetchData();
 });
-
-let next = document.getElementById("next")
-    next.addEventListener("click", () => {
-  updatePage(CurrPage + 1);
-});
-
-function themeSelector() {
-  const theme = document.getElementById("theme").value;
-  document.querySelector("body").style.backgroundColor = theme;
-  showData(CurrPage);
-}
-
-function Random(){
-    const Randombtn=document.getElementById("random")
-    Randombtn.addEventListener("click",async()=>{
-      window.location.href="random.html"
-    })
-}
-
-Random()
-document.getElementById("theme").addEventListener("change", themeSelector);
-
-fetchData();
-})
-const clock=document.getElementById("time")
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["January", "February", "March", "April", "May", "June", 
-                "July", "August", "September", "October", "November", "December"];
+const clock = document.getElementById("time");
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 setInterval(() => {
-    const date=new Date
-    const day=date.getDay()
-    const month=months[date.getMonth()]
-    const year=date.getFullYear()
-    const weekDay=days[date.getDay()]
-    const second=date.getSeconds()
-    const minute=date.getMinutes()
-    const hour=date.getHours()
+  const date = new Date();
+  const day = date.getDay();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+  const weekDay = days[date.getDay()];
+  const second = date.getSeconds();
+  const minute = date.getMinutes();
+  const hour = date.getHours();
 
-    clock.innerText=`${hour}:${minute}:${second} - ${weekDay} - ${day+1} ${month} ${year}`
-
+  clock.innerText = `${hour}:${minute}:${second} - ${weekDay} - ${
+    day + 1
+  } ${month} ${year}`;
 }, 1000);
